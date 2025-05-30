047afd3d65e8ddfdf5baa8ebfc5d978acaea8b67
YdrMaster <ydrml@hotmail.com>
2025-05-30 10:09:45 +0800

    build: 适配沐曦
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- llama.cu/build.rs
+++ llama.cu/build.rs
@@ -1,13 +1,4 @@
 ﻿fn main() {
     use build_script_cfg::Cfg;
-    use search_cuda_tools::{find_cuda_root, find_nccl_root};
-
-    assert!(
-        find_cuda_root().is_some(),
-        "cuda not found, check $CUDA_ROOT env var"
-    );
-    let nccl = Cfg::new("nccl");
-    if find_nccl_root().is_some() {
-        nccl.define()
-    }
+    Cfg::new("nccl").define()
 }
